9ae5957ccf65e7aee08d79a563c5c0a9e809af03
traketal <alanmritchie@gmail.com>
2019-01-23 14:09:36 -0700

    Update nomp.sh

--- nomp.sh
+++ nomp.sh
@@ -17,6 +17,14 @@ function EPHYMERAL_PORT(){
     done
 }
 
+veil-cli stop
+sleep 2
+seed="$(veild -generateseed=1)"
+sleep 10
+veil-cli stop
+sleep 2
+veild -daemon=1
+
 echo "Making the NOMPness Monster"
 
 cd $STORAGE_ROOT/nomp/site/
@@ -26,14 +34,14 @@ npm install
 npm i npm@latest -g
 
 # SED config file
-sudo cp -r $HOME/veilpool/config.json $STORAGE_ROOT/nomp/site/config.json
+sudo cp -r $HOME/veilpool/install/config.json $STORAGE_ROOT/nomp/site/config.json
 sudo sed -i 's/PUBIP/'$PUBLIC_IP'/g' config.json
 sudo sed -i 's/FQDN/'$StratumURL'/g' config.json
 sudo sed -i 's/PASSWORD/'$AdminPass'/g' config.json
 
 # Create the coin json file
 cd $STORAGE_ROOT/nomp/site/pool_configs
-sudo cp -r $HOME/veilpool/installbase_samp.json.x $STORAGE_ROOT/nomp/site/pool_configs/veil.json
+sudo cp -r $HOME/veilpool/install/base_samp.json.x $STORAGE_ROOT/nomp/site/pool_configs/veil.json
 
 # Generate our random ports
 randportlow=$(EPHYMERAL_PORT)
@@ -41,12 +49,7 @@ randportvar=$(EPHYMERAL_PORT)
 randporthigh=$(EPHYMERAL_PORT)
 
 #Generate new wallet address
-/usr/bin/veil-cli stop
-sleep 2
-seed="$(veild -generateseed=1)"
-sleep 2
-/usr/bin/veil-cli stop
-sleep 2
+
 wallet="$(veil-cli -datadir=$STORAGE_ROOT/wallets/.veil -conf=veil.conf getnewbasecoinaddress)"
 
 
