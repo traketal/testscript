6c52c4fb4601e4f777f33856be2679c8fb1863eb
traketal <alanmritchie@gmail.com>
2019-01-22 21:55:57 -0700

    Create startnomp.sh

---
+++ startnomp.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+source /etc/functions.sh
+source /etc/veilpool.conf
+
+# Create the temporary installation directory if it doesn't already exist.
+echo Creating the temporary NOMP installation folder...
+if [ ! -d $STORAGE_ROOT/nomp/nomp_setup ]; then
+sudo mkdir -p $STORAGE_ROOT/nomp/nomp_setup
+sudo mkdir -p $STORAGE_ROOT/nomp/nomp_setup/tmp
+sudo mkdir -p $STORAGE_ROOT/nomp/site
+sudo mkdir -p $STORAGE_ROOT/nomp/starts
+sudo mkdir -p $STORAGE_ROOT/wallets
+sudo mkdir -p $HOME/multipool/daemon_builder
+fi
+sudo setfacl -m u:$USER:rwx $STORAGE_ROOT/nomp
+sudo setfacl -m u:$USER:rwx $STORAGE_ROOT/nomp/site
+sudo setfacl -m u:$USER:rwx $STORAGE_ROOT/wallets
+sudo setfacl -m u:$USER:rwx $STORAGE_ROOT/nomp/nomp_setup/tmp
+
+# Start the installation.
+source questions.sh
+source system.sh
+source db.sh
+source web.sh
+#source daemon.sh
+#source build_coin.sh
+source nomp.sh
+source motd.sh
+source server_harden.sh
+source server_cleanup.sh
+
+clear
+echo Installation of your NOMP single server is now completed.
+echo You *MUST* reboot the machine to finalize the machine updates and folder permissions! NOMP will not function until a reboot is performed!
+echo
+echo
+echo By default all stratum ports are blocked by the firewall. To allow a port through, from the command prompt type sudo ufw allow port number.
