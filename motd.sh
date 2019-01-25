6448aeced9933fc00f2a78cd96b3eadec321ae68
traketal <alanmritchie@gmail.com>
2019-01-24 17:57:22 -0700

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -2,13 +2,13 @@ source /etc/functions.sh # load our functions
 
 apt_install lsb-release figlet update-motd \
 landscape-common update-notifier-common
-cd $HOME/veilpool/nomp/ubuntu/etc/update-motd.d
+cd $HOME/veilpool/install//ubuntu/etc/update-motd.d
 sudo rm -r /etc/update-motd.d/
 sudo mkdir /etc/update-motd.d/
 sudo touch /etc/update-motd.d/00-header ; sudo touch /etc/update-motd.d/10-sysinfo ; sudo touch /etc/update-motd.d/90-footer
 sudo chmod +x /etc/update-motd.d/*
 sudo cp -r 00-header 10-sysinfo 90-footer /etc/update-motd.d/
-cd $HOME/veilpool/nomp/ubuntu
+cd $HOME/veilpool/install/ubuntu
 sudo cp -r nomp /usr/bin/
 sudo chmod +x /usr/bin/nomp
 echo '
